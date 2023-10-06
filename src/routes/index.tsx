7933f69279276226988915124c65f9367496f29d
Carlos Eduardo <carloseduardo.na7@gmail.com>
2023-10-06 10:22:48 -0300

    att

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página inicial</p>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial"/>}/>
+    </Routes>
+  );
+};
+
+export default AppRoutes;
